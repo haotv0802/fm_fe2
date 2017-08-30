b13557be561115525a996c2af0243cb9e94370fc
HaoHo <haho@codix.eu>
2017-08-30 09:59:53 +0700

    How to add CSS.

--- App.jsx
+++ App.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 class App extends React.Component {
   render() {
+    var myStyle = {
+      fontSize: 100,
+      color: '#FF0000'
+    }
     return (
-      <div>
+      <div style={myStyle}>
         Hello World!!!
         Hello Hao
       </div>
