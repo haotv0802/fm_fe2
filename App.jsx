9eb606fca7e244a03b121ca4783a37c5d3622c40
HaoHo <haho@codix.eu>
2017-08-30 10:02:02 +0700

    How to put data to Table.

--- App.jsx
+++ App.jsx
@@ -1,16 +1,66 @@
 import React from 'react';
 
 class App extends React.Component {
-  render() {
-    var myStyle = {
-      fontSize: 100,
-      color: '#FF0000'
+  constructor() {
+    super();
+
+    this.state = {
+      data:
+        [
+          {
+            "id":1,
+            "name":"Foo",
+            "age":"20"
+          },
+
+          {
+            "id":2,
+            "name":"Bar",
+            "age":"30"
+          },
+
+          {
+            "id":3,
+            "name":"Baz",
+            "age":"40"
+          }
+        ]
     }
+  }
+
+  render() {
     return (
-      <div style={myStyle}>
-        Hello World!!!
-        Hello Hao
+      <div>
+        <Header/>
+        <table>
+          <tbody>
+          {this.state.data.map((person, i) => <TableRow key = {i}
+                                                        data = {person} />)}
+          </tbody>
+        </table>
       </div>
+    );
+  }
+}
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Header</h1>
+      </div>
+    );
+  }
+}
+
+class TableRow extends React.Component {
+  render() {
+    return (
+      <tr>
+        <td>{this.props.data.id}</td>
+        <td>{this.props.data.name}</td>
+        <td>{this.props.data.age}</td>
+      </tr>
     );
   }
 }
