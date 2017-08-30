6b6f86f1e88bc5bacfe2b954f21b2866a765cb08
HaoHo <haho@codix.eu>
2017-08-30 14:56:08 +0700

    Applying react-router.

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.jsx';
-import Home from "./Home.jsx";
-import About from "./About.jsx";
-import Contact from "./Contact.jsx";
+import App from './app/App.jsx';
+import Home from "./app/Home.jsx";
+import About from "./app/About.jsx";
+import Contact from "./app/Contact.jsx";
 import { Router, Route, Link, browserHistory, IndexRoute, hashHistory } from 'react-router';
 
 ReactDOM.render((
