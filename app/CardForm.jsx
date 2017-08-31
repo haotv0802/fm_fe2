abb4b484e7175a2ded1fffc85ab93298140d0767
HaoHo <haho@codix.eu>
2017-08-31 11:44:42 +0700

    Apply "ref".

--- app/CardForm.jsx
+++ app/CardForm.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 
 class CardForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault;
+    console.log("submit clicked.");
+    console.log(this.textInput.value);
+  }
+
   render() {
     return (
-      <form>
-        <input type="text" placeholder="Type Username here." />
+      <form onSubmit={this.handleSubmit}>
+        <input
+          ref={(input) => { this.textInput = input; }}
+          type="text" placeholder="Type Username here." />
         <button type="submit">Add card</button>
       </form>
     )
