d5dc5c4083f1b32264fea2ed1a4b95e5eb68ba1d
HaoHo <haho@codix.eu>
2017-08-31 11:50:03 +0700

    Apply setState.

--- app/CardForm.jsx
+++ app/CardForm.jsx
@@ -3,20 +3,28 @@ import React from 'react';
 class CardForm extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      username : 'user name ....'
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleOnChange = this.handleOnChange.bind(this);
   }
 
   handleSubmit(event) {
     event.preventDefault;
     console.log("submit clicked.");
-    console.log(this.textInput.value);
+    console.log(this.state.username);
+  }
+
+  handleOnChange(event) {
+    this.setState({username: event.target.value});
   }
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <input
-          ref={(input) => { this.textInput = input; }}
+          value = {this.state.username} onChange={this.handleOnChange}
           type="text" placeholder="Type Username here." />
         <button type="submit">Add card</button>
       </form>
