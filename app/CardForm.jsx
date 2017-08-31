f3f1ff1bb13bba9e8b2a3e681ae5ae3023482c73
HaoHo <haho@codix.eu>
2017-08-31 12:00:43 +0700

    Add axios & react-axios.

--- app/CardForm.jsx
+++ app/CardForm.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Axios from 'axios';
 
 class CardForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      username : 'user name ....',
+      username : 'jordwalke',
       age: 20
     };
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -13,9 +14,13 @@ class CardForm extends React.Component {
 
   handleSubmit(event) {
     event.preventDefault;
-    console.log("submit clicked.");
-    console.log(this.state.username);
-    console.log(this.state.age);
+    // console.log("submit clicked.");
+    // console.log(this.state.username);
+    // console.log(this.state.age);
+    Axios.get(`https://api.github.com/users/${this.state.username}`).then
+    (resp => {
+      console.log(resp);
+    });
   }
 
   handleOnChange(event) {
@@ -26,7 +31,8 @@ class CardForm extends React.Component {
     return (
       <form onSubmit={this.handleSubmit}>
         <input
-          value = {this.state.username} onChange={this.handleOnChange}
+          // onChange={this.handleOnChange}
+          value = {this.state.username} onChange={(event) => this.setState({username: event.target.value})}
           type="text" placeholder="Type Username here." />
         <button type="submit">Add card</button>
       </form>
